db359d71c99c63fac5034fc52400bfdd9aac5cbb
MegaMax42rus <megamax42rus@gmail.com>
2023-02-15 23:18:54 +0400

    Test 1

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const gh_token = core.getInput('gh_token');
+
 try {
-  // `who-to-greet` input defined in action metadata file
-  const gh_token = core.getInput('gh_token');
   console.log(`Hello ${gh_token}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+
+  const after = github.context.payload.after;
+  const ref = github.context.payload.ref;
+  console.log(`After: ${after} Ref: ${ref}`);
 } catch (error) {
   core.setFailed(error.message);
 }
