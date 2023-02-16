1c88ed83cd70e70892b1cb2a99cc42194b3cd65c
MegaMax42rus <megamax42rus@gmail.com>
2023-02-16 23:05:35 +0400

    Test 17

--- dist/index.js
+++ dist/index.js
@@ -9785,7 +9785,7 @@ try {
   console.log(`github.context: ${context}`);
 
   const sha = github.context.sha;
-  const branch = github.context.ref.match(/^refs\/heads\/(.*)/g)[1];
+  const branch = github.context.ref.match(/^refs\/heads\/(.*)/ig);
   console.log(`Sha: ${sha} Branch: ${branch}`);
 
   const ref = 'tags/v1.1'
