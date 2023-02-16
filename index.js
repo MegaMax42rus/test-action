cdad988b61669d499bea0da8b2757f87a41bbc15
MegaMax42rus <megamax42rus@gmail.com>
2023-02-17 00:40:51 +0400

    Test

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-async function get_tags(octokit) {
-  const tags_ref = 'tags'
-  const tags_detailt = await octokit.rest.git.listMatchingRefs({
-    ...github.context.repo,
-    tags_ref
-  });
-  console.log(tags_detailt.data);
+async function f() {
+  try {
+    const gh_token = core.getInput('gh_token');
+    const octokit = github.getOctokit(gh_token);
+
+    //const context = JSON.stringify(github.context, undefined, 2);
+    //console.log(`github.context: ${context}`);
+
+    const sha = github.context.sha;
+    const branch = github.context.ref.match(/^refs\/heads\/(.*)/)[1];
+    console.log(`Sha: ${sha}\nBranch: ${branch}`);
+
+    // Get refs/tags
+    const tags_ref = 'tags'
+    const tags_detailt = await octokit.rest.git.listMatchingRefs({
+      ...github.context.repo,
+      tags_ref
+    });
+    console.log(tags_detailt.data);
+
+    const tags = tags_detailt.data;
+
+    //tags_detailt.then(function(result) {
+    //  tags = result.data;
+    //})
+
+    console.log(JSON.stringify(tags, undefined, 2));
+
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
-try {
-  const gh_token = core.getInput('gh_token');
-  const octokit = github.getOctokit(gh_token);
-
-  //const context = JSON.stringify(github.context, undefined, 2);
-  //console.log(`github.context: ${context}`);
-
-  const sha = github.context.sha;
-  const branch = github.context.ref.match(/^refs\/heads\/(.*)/)[1];
-  console.log(`Sha: ${sha}\nBranch: ${branch}`);
-
-  // Get refs/tags
-  const tags = get_tags(octokit);
-
-  //tags_detailt.then(function(result) {
-  //  tags = result.data;
-  //})
-
-  console.log(JSON.stringify(tags, undefined, 2));
-
-
-} catch (error) {
-  core.setFailed(error.message);
-}
+f();
