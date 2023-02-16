83ed3628c08d578f0ef2183586069a5d26930460
MegaMax42rus <megamax42rus@gmail.com>
2023-02-17 01:54:27 +0400

    Test

--- index.js
+++ index.js
@@ -40,8 +40,7 @@ async function f() {
       if (branch == 'master' || branch == 'main') {
         tag_ref_regex = /^refs\/tags\/v(\d+\.\d+\.\d+)$/;
         console.log('master/main');
-      }
-      if (branch.search(/release\//) >= 0 || branch.search(/releases\//) >= 0) {
+      } else if (branch.search(/release\//) >= 0 || branch.search(/releases\//) >= 0) {
         tag_ref_regex = /^refs\/tags\/v(\d+\.\d+\.\d+-rc\d+)$/;
         console.log('release/releases');
       }
