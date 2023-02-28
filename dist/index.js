d306b44eba353f3d8308b2765fa52c8bf10f22c4
MegaMax42rus <megamax42rus@gmail.com>
2023-02-28 21:21:06 +0400

    Test

--- dist/index.js
+++ dist/index.js
@@ -9815,11 +9815,10 @@ async function f() {
     // Getting the value of the last tag
     if (need_add_tag) {
       console.log('NEED ADD TAG');
-      console.log(`${branch.search(/^Areleases?\/\d+\.\d+\.[\dx]+$/)}`);
       if (branch == 'master' || branch == 'main') {
         tag_ref_regex = /^refs\/tags\/v(\d+\.\d+\.)(\d+)$/;
         console.log('master/main');
-      } else if (branch.search(/^releases?\/\d+\.\d+\.[\dx]+$/) > 0) {
+      } else if (branch.search(/^releases?\/\d+\.\d+\.[\dx]+$/) >= 0) {
 
         tag_ref_regex = /^refs\/tags\/v(\d+\.\d+\.\d+-rc)(\d+)$/;
         console.log('release/releases');
