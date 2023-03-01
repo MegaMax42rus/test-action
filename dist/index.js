04fa359c9bbacbb3933a8bf124d2e19dd3dcae52
MegaMax42rus <megamax42rus@gmail.com>
2023-03-01 19:22:57 +0400

    Test

--- dist/index.js
+++ dist/index.js
@@ -9832,18 +9832,23 @@ async function f() {
 
     if (mode == 'release/releases') {
       console.log('Enter in release/releases mode')
-      let max_rc_vercion = get_max_tag(all_tags, /v\d+\.\d+\.\d+-rc\d+$/)
+      let max_rc_vercion = get_max_tag(all_tags, /v\d+\.\d+\.\d+-rc\d+$/);
       if (max_rc_vercion) {
         console.log(`Max rc vercion: ${max_rc_vercion}`);
-        let max_clear_vercion = get_max_tag(all_tags, /v\d+\.\d+\.\d+$/)
-        console.log(`Max clear vercion: ${max_clear_vercion}`);
+        let max_clear_vercion = get_max_tag(all_tags, /v\d+\.\d+\.\d+$/);
+        if (max_clear_vercion) {
+          console.log(`Max clear vercion: ${max_clear_vercion}`);
+          if (max_rc_vercion.match(/v(\d+\.\d+\.\d+)-rc\d+$/)[1] == max_clear_vercion) {
+            console.log('increment path +rc0')
+          } else {
+            console.log('else')
+          }
+        } else {
+          console.log('increment rc')
+        }
+
       }
     }
-
-
-
-
-
 
 
 
