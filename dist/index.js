945e4e8aac96fbc2e5c60f827fe8b7214c0b5e83
MegaMax42rus <megamax42rus@gmail.com>
2023-03-01 21:25:54 +0400

    Test

--- dist/index.js
+++ dist/index.js
@@ -9792,6 +9792,18 @@ function get_max_tag(tag_array, regex) {
   return max_tag;
 }
 
+function get_max_tag_match(tag_array, regex) {
+  var max_tag;
+  for (tag in tag_array) {
+    try {
+      max_tag = tag_array[tag].match(regex)[1];
+    } catch (error) {
+      continue;
+    }
+  }
+  return max_tag;
+}
+
 function increment_patch(version) {
   let split_version = version.match(/v?(\d+\.\d+\.)(\d+)$/);
   return `${split_version[1]}${parseInt(split_version[2])+1}`;
@@ -9800,6 +9812,24 @@ function increment_patch(version) {
 function increment_rc(version) {
   let split_version = version.match(/v?(\d+\.\d+\.\d+-rc)(\d+)$/);
   return `${split_version[1]}${parseInt(split_version[2])+1}`;
+}
+
+function release_mode(release, version) {
+  if (version) {
+    //
+  } else {
+    let max_version_regex = new RegExp(`^(v${release.replace('.','\\.')}\\.\\d+)\\.*$`);
+    let max_version = get_max_tag_match(all_tags, max_version_regex);
+    console.log(`Max version: ${max_clear_version}`);
+
+    let max_clear_version_regex = new RegExp(`^v${release.replace('.','\\.')}\\.\\d+$`);
+    let max_clear_version = get_max_tag(all_tags, max_clear_version_regex);
+    console.log(`Max clear version: ${max_clear_version}`);
+
+    let max_rc_version_regex = new RegExp(`^v${release.replace('.','\\.')}\\.\\d+-rc\\d+$`);
+    let max_rc_version = get_max_tag(all_tags, max_rc_version_regex);
+    console.log(`Max rc version: ${max_rc_version}`);
+  }
 }
 
 async function f() {
@@ -9829,42 +9859,17 @@ async function f() {
     // Select mode
     var mode;
     if (branch == 'master' || branch == 'main') {
-      mode = 'master/main';
+      console.log('Rule for: master/main');
     } else if (branch.search(/^releases?\/\d+\.\d+\.[\dx]+$/) >= 0) {
-      mode = 'release/releases';
-    } else {
-      core.setFailed(`No rule for brunch "${branch}"`);
-    }
-    console.log(`Rule for: ${mode}`);
-
-    if (mode == 'release/releases') {
+      console.log('Rule for: release/releases');
       let release = branch.match(/^releases?\/(\d+\.\d+)\.[\dx]+$/)[1];
       release = '1.2';
       console.log(`Release version: ${release}`);
-      let max_rc_vercion_regex = new RegExp(`^v${release.replace('.','\\.')}\\.\\d+-rc\\d+$`);
-      let max_rc_vercion = get_max_tag(all_tags, max_rc_vercion_regex);
-      if (max_rc_vercion) {
-        console.log(`Max rc version: ${max_rc_vercion}`);
-        let max_clear_vercion_regex = new RegExp(`^v${release.replace('.','\\.')}\\.\\d+$`);
-        let max_clear_vercion = get_max_tag(all_tags, max_clear_vercion_regex);
-        if (max_clear_vercion) {
-          console.log(`Max clear version: ${max_clear_vercion}`);
-          if (max_rc_vercion.match(/(v\d+\.\d+\.\d+)-rc\d+$/)[1] == max_clear_vercion) {
-            new_tag = `${increment_patch(max_clear_vercion)}-rc0`;
-          } else {
-            new_tag = increment_rc(max_rc_vercion);
-          }
-        } else {
-          new_tag = increment_rc(max_rc_vercion);
-        }
-      } else {
-        new_tag = `v${release}.0-rc0`;
-      }
+      new_tag = release_mode(release, null);
       console.log(`New tag: ${new_tag}`)
+    } else {
+      core.setFailed(`No rule for brunch "${branch}"`);
     }
-
-
-
   } catch (error) {
     core.setFailed(error.message);
   }
