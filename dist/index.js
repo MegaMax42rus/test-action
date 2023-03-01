3abd23151e7a350c6f51efc9049d421f9a10d1e6
MegaMax42rus <megamax42rus@gmail.com>
2023-03-01 18:07:26 +0400

    Test

--- dist/index.js
+++ dist/index.js
@@ -9799,12 +9799,14 @@ async function f() {
     for (tag in all_tags_detailt.data) {
       all_tags.push(all_tags_detailt.data[tag].ref);
       if (debug) {
-        let tag_ref = all_tags_detailt.data[tag].ref;
+        let tag_ref = all_tags_detailt.data[tag].ref.match(/^refs\/tags\/(.*)/)[1];
         let tag_sha = all_tags_detailt.data[tag].object.sha;
         console.log(`${tag_ref} (${tag_sha})`);
       }
-
     }
+
+
+
 
 
 
